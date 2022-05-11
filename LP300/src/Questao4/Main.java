c460e49e4a5681ca50e3ee946bcd11bd35f615f6
maaluoliv <84928556+maaluoliv@users.noreply.github.com>
2022-05-11 18:44:11 -0300

    Add files via upload

---
+++ LP300/src/Questao4/Main.java
@@ -0,0 +1,40 @@
+package Questao4;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int segundos;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um valor em segundos: ");
+        segundos = ler.nextInt();
+        System.out.println("O valor em horas: "+ segundos/3600);
+        System.out.println("O valor em minutos: "+ segundos/60);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
